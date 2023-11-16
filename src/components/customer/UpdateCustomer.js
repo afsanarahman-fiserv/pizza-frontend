b717d6877dd608e9e56f0ce3c276b1767001a99f
alicia-john <alicia.john@fiserv.com>
2023-11-16 11:33:42 -0500

    hjgbk

---
+++ src/components/customer/UpdateCustomer.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import CustomerService from "../service/CustomerService"
+export default function UpdateCustomer(){
+    let location  = useLocation();
+    let index = location.state.phone_number
+
+    let [customers, setCustomer] = useState({
+        phone_number : '',
+        name : '',
+        street_address : '',
+        zip_code : ''
+
+    });
+
+    useEffect(() =>{
+        CustomerService.f
+
+    }, []);
+
+    let [phone_number, setPhone_Number] = useState('');
+    let [name, setName] = useState('');
+    let [street_address, setAddress] = useState('');
+    let [zip_code, setZip_Code] = useState('');
+
+    let handleNumber = (e) => { setPhone_Number(e.target.value) }
+    let handleName = (e) => { setName(e.target.value) }
+    let handleAddress = (e) => { setAddress(e.target.value) }
+    let handleZip = (e) => { setZip_Code(e.target.value) }
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        let customer = {phone_number : phone_number, name : name, street_address : street_address, zip_code : zip_code}
+        CustomerService.addEmployee(customer).then(()=>{
+            alert("Employee added successfully")
+        }, ()=>{
+            alert("Employee creation failed")
+        });
+    }
+
+    return(
+        <>
+        <h3>Adding New Customer</h3>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Phone Number: <input onChange={handleNumber} type="text" value={phone_number}/>
+            </label>
+            <br/>
+            <label>
+                Name: <input onChange={handleName} type="text" value={name}/>
+            </label>
+            <br/>
+            <label>
+                Street Address: <input onChange={handleAddress} type="text" value={street_address}/>
+            </label>
+            <br/>
+            <label>
+                Zip Code: <input onChange={handleZip} type="text" value={zip_code}/>
+            </label>
+            <br/>
+            <input type="submit" value="Submit"/>
+        </form>
+        </>
+    );
+ }
