02ababa2fb363cb562954517f2258d36c6eb06ad
alicia-john <alicia.john@fiserv.com>
2023-11-17 15:36:15 -0500

    customer_stuff

--- src/service/CustomerService.js
+++ src/service/CustomerService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // DOUBLE CHECK IF URL IS CORRECT
 const getAllCustomersUrl = "http://localhost:8080/api/customers";
-const getCustomerUrl = "http://localhost:8080/api/customer"
+const getCustomerUrl = "http://localhost:8080/api/customers"
 const addCustomerUrl = "http://localhost:8080/api/addCustomer";
 const updateCustomerUrl = "http://localhost:8080/api/updateCustomer";
 const deleteCustomerUrl = "http://localhost:8080/api/deleteCustomer";
@@ -19,6 +19,7 @@ class CustomerService {
         return axios.post(addCustomerUrl, customer);
     }
     findCustomer(phone_number){
+        console.log(getCustomerUrl + phone_number)
         return axios.get(getCustomerUrl + phone_number);
     }
     updateCustomer(customer) {
